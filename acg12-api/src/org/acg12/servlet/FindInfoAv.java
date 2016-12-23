9443ff18dd2f86a564607805aec54a31710135fc
hui793114465 <hui793114465@163.com>
2016-12-23 11:10:59 +0800

    v1.0.1 api

---
+++ acg12-api/src/org/acg12/servlet/FindInfoAv.java
@@ -0,0 +1,37 @@
+package org.acg12.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.acg12.utils.ReptileUtils;
+
+public class FindInfoAv extends HttpServlet {
+
+	private static final long serialVersionUID = 4691720265709695445L;
+
+	public FindInfoAv() {
+		super();
+	}
+	
+	public void init() throws ServletException {
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ReptileUtils.getFindInfoAv(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+        doGet(request, response);
+	}
+
+	public void destroy() {
+		super.destroy(); 
+	}
+	
+}
